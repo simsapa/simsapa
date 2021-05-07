84eeb39ea07ebcc4d21661b916fa097b96b0de21
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2021-05-07 19:50:17 +0100

    ui with icons

--- simsapa/layouts/sutta_search.py
+++ simsapa/layouts/sutta_search.py
@@ -1,83 +1,31 @@
 from functools import partial
 from typing import List
 
-from PyQt5.QtCore import Qt  # type: ignore
-from PyQt5.QtWidgets import (QHBoxLayout, QLabel, QLineEdit,  # type: ignore
-                             QMainWindow, QPushButton, QTextBrowser, QListWidget,
-                             QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QLabel, QMainWindow)  # type: ignore
 
 from ..app.db_models import RootText as DbSutta  # type: ignore
-from ..app.types import AppData, Sutta  # type: ignore
+from ..app.types import (AppData, Sutta)  # type: ignore
+from ..assets.ui.sutta_search_window_ui import Ui_SuttaSearchWindow  # type: ignore
 
 
-class SuttaSearchWindow(QMainWindow):
-    def __init__(self, app_data: AppData) -> None:
-        super().__init__()
-        self.setWindowTitle('Simsapa - Sutta Search')
+class SuttaSearchWindow(QMainWindow, Ui_SuttaSearchWindow):
+    def __init__(self, app_data: AppData, parent=None) -> None:
+        super().__init__(parent)
+        self.setupUi(self)
 
         self._app_data: AppData = app_data
-        self._sutta_results: List[Sutta] = []
-        self._sutta_history: List[Sutta] = []
+        self._results: List[Sutta] = []
+        self._history: List[Sutta] = []
 
-        self.layout = QVBoxLayout()
-        self._central_widget = QWidget(self)
-        self.setCentralWidget(self._central_widget)
+        self._ui_setup()
 
-        self._central_widget.setLayout(self.layout)
+        self.statusbar.showMessage("Ready", 3000)
 
-        self._create_window_layout()
+    def _ui_setup(self):
+        self.status_msg = QLabel("Sutta title")
+        self.statusbar.addPermanentWidget(self.status_msg)
 
-    def _create_window_layout(self):
-        self._create_search_bar()
-        self._create_results()
-
-    def _create_search_bar(self):
-        layout = QHBoxLayout()
-
-        w = QLineEdit()
-        w.setFixedHeight(35)
-        w.setAlignment(Qt.AlignLeft)
-        w.setFocus()
-
-        self.search_input = w
-        layout.addWidget(self.search_input)
-
-        w = QPushButton('Search')
-        w.setFixedSize(100, 40)
-
-        self.search_button = w
-        layout.addWidget(self.search_button)
-
-        self.layout.addLayout(layout)
-
-    def _create_results(self):
-        layout = QHBoxLayout()
-
-        w = QTextBrowser()
-
-        self.html_frame = w
-        layout.addWidget(self.html_frame)
-
-        results_history_layout = QVBoxLayout()
-
-        w = QLabel('Results')
-        results_history_layout.addWidget(w)
-
-        self.sutta_results = QListWidget()
-        results_history_layout.addWidget(self.sutta_results)
-
-        w = QLabel('History')
-        results_history_layout.addWidget(w)
-
-        self.sutta_history = QListWidget()
-        results_history_layout.addWidget(self.sutta_history)
-
-        layout.addLayout(results_history_layout)
-
-        self.layout.addLayout(layout)
-
-    def set_html_content(self, html):
-        self.html_frame.setText(html)
+        self.search_input.setFocus()
 
 
 class SuttaSearchCtrl:
@@ -88,25 +36,30 @@ class SuttaSearchCtrl:
     def _handle_query(self):
         query = self._view.search_input.text()
         if len(query) > 3:
-            self._view._sutta_results = self._sutta_search_query(query)
-            titles = list(map(lambda s: s.title, self._view._sutta_results))
-            self._view.sutta_results.clear()
-            self._view.sutta_results.addItems(titles)
+            self._view._results = self._sutta_search_query(query)
+            titles = list(map(lambda s: s.title, self._view._results))
+            self._view.results_list.clear()
+            self._view.results_list.addItems(titles)
+
+    def _set_content_html(self, html):
+        self._view.content_html.setText(html)
 
     def _handle_result_select(self):
-        selected_idx = self._view.sutta_results.currentRow()
-        sutta: Sutta = self._view._sutta_results[selected_idx]
+        selected_idx = self._view.results_list.currentRow()
+        sutta: Sutta = self._view._results[selected_idx]
         self._show_sutta(sutta)
 
-        self._view._sutta_history.insert(0, sutta)
-        self._view.sutta_history.insertItem(0, sutta.title)
+        self._view._history.insert(0, sutta)
+        self._view.history_list.insertItem(0, sutta.title)
 
     def _handle_history_select(self):
-        selected_idx = self._view.sutta_history.currentRow()
-        sutta: Sutta = self._view._sutta_history[selected_idx]
+        selected_idx = self._view.history_list.currentRow()
+        sutta: Sutta = self._view._history[selected_idx]
         self._show_sutta(sutta)
 
     def _show_sutta(self, sutta: Sutta):
+        self._view.status_msg.setText(sutta.title)
+
         html = """
 <!doctype html>
 <html>
@@ -120,7 +73,7 @@ class SuttaSearchCtrl:
 </html>
 """ % ('', sutta.content_html)
 
-        self._view.set_html_content(html)
+        self._set_content_html(html)
 
     def _sutta_search_query(self, query: str):
         results = self._view._app_data.db_session \
@@ -130,8 +83,10 @@ class SuttaSearchCtrl:
         return results
 
     def _connect_signals(self):
+        self._view.action_Quit.triggered.connect(partial(self._view.close))
+
         self._view.search_button.clicked.connect(partial(self._handle_query))
         self._view.search_input.textChanged.connect(partial(self._handle_query))
         # self._view.search_input.returnPressed.connect(partial(self._update_result))
-        self._view.sutta_results.itemSelectionChanged.connect(partial(self._handle_result_select))
-        self._view.sutta_history.itemSelectionChanged.connect(partial(self._handle_history_select))
+        self._view.results_list.itemSelectionChanged.connect(partial(self._handle_result_select))
+        self._view.history_list.itemSelectionChanged.connect(partial(self._handle_history_select))
