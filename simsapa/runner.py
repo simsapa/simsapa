fa4d735eb58074b7f154ec3511b975c99e503590
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2021-05-08 08:31:47 +0100

    systray icon

--- simsapa/runner.py
+++ simsapa/runner.py
@@ -5,10 +5,13 @@ import logging as _logging
 import logging.config
 import yaml
 
-from PyQt5.QtWidgets import QApplication  # type: ignore
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QSystemTrayIcon, QMenu, QAction)  # type: ignore
 
 from .app.types import AppData  # type: ignore
 from .app.windows import AppWindows  # type: ignore
+
+from simsapa.assets import icons_rc
 
 logger = _logging.getLogger(__name__)
 
@@ -39,6 +42,22 @@ def main():
     app_data = AppData(db_path)
 
     app = QApplication(sys.argv)
+
+    # Create systray
+    app.setQuitOnLastWindowClosed(False)
+
+    tray = QSystemTrayIcon(QIcon(":bookmark"))
+    tray.setVisible(True)
+
+    menu = QMenu()
+
+    ac = QAction(QIcon(":close"), "Quit")
+    ac.triggered.connect(app.quit)
+    menu.addAction(ac)
+
+    tray.setContextMenu(menu)
+
+    # Create first window
 
     app_windows = AppWindows(app_data)
     app_windows._new_sutta_search_window()
