462dc14a14ccb54b60c10c6bd82d6e594b30242a
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2022-02-21 09:34:39 +0000

    use flask instead of falcon

--- simsapa/app/api.py
+++ simsapa/app/api.py
@@ -1,59 +1,74 @@
-import json
 import socket
-from typing import Optional
-import falcon
-from wsgiref.simple_server import make_server
+import os
+from flask import Flask, send_from_directory, abort, request
+from flask.wrappers import Response
+from flask_cors import CORS
 
 from simsapa import APP_QUEUES, PACKAGE_ASSETS_DIR
 from simsapa import logger
 
-class QueueResource:
-    # def on_get(self, req: falcon.Request, resp: falcon.Response):
-    #     logger.error("Resp: 404 Not Found")
-    #     resp.status = falcon.HTTP_404
+app = Flask(__name__)
+app.config['ENV'] = 'development'
+cors = CORS(app)
 
-    def on_post(self, req: falcon.Request, resp: falcon.Response, queue_id: Optional[str]):
-        if req.content_type == 'application/json':
-            m = req.get_media(default_when_empty="{}")
-            data = json.dumps(m)
-            logger.info("QueueResource.on_post() data: %s" % data)
+@app.route('/queues/<string:queue_id>', methods=['POST'])
+def queues(queue_id):
+    if request.content_type == 'application/json':
+        try:
+            # NOTE get the byte string, we want a string as the queue message,
+            # and we'll deserialize somewhere else, instead of doing so with
+            # .get_json() here.
+            data = request.get_data(as_text=True, cache=False)
+        except Exception as e:
+            abort(Response(f"{e}", 403))
 
-        else:
-            logger.error("Resp: 400 Bad Request")
-            resp.status = falcon.HTTP_400
-            return
+        logger.info(f"QueueResource.on_post() data: {data}")
 
-        if queue_id is None:
-            logger.error("Resp: 404 Not Found")
-            resp.status = falcon.HTTP_404
-            return
+    else:
+        abort(400)
 
-        if queue_id != 'all' and queue_id not in APP_QUEUES.keys():
-            logger.error("Resp: 403 Forbidden")
-            resp.status = falcon.HTTP_403
-            return
+    if queue_id is None:
+        abort(404)
 
-        if queue_id == 'all':
-            for i in APP_QUEUES.keys():
-                APP_QUEUES[i].put_nowait(data)
-        else:
-            APP_QUEUES[queue_id].put_nowait(data)
+    if queue_id != 'all' and queue_id not in APP_QUEUES.keys():
+        abort(403)
 
-        logger.info("Resp: 200 OK")
-        resp.status = falcon.HTTP_200
+    if queue_id == 'all':
+        for i in APP_QUEUES.keys():
+            APP_QUEUES[i].put_nowait(data)
+    else:
+        APP_QUEUES[queue_id].put_nowait(data)
+
+    return 'OK', 200
+
+@app.route('/assets/<path:path>', methods=['GET'])
+def assets(path):
+    if not os.path.isfile(os.path.join(PACKAGE_ASSETS_DIR, path)):
+        abort(404)
+
+    return send_from_directory(PACKAGE_ASSETS_DIR, path)
+
+@app.errorhandler(400)
+def resp_bad_request(e):
+    msg = f"Bad Request: {e}"
+    logger.error(msg)
+    return msg, 400
+
+@app.errorhandler(404)
+def reps_not_found(e):
+    msg = f"Not Found: {e}"
+    logger.error(msg)
+    return msg, 404
+
+@app.errorhandler(403)
+def resp_forbidden(e):
+    msg = f"Forbidden: {e}"
+    logger.error(msg)
+    return msg, 403
 
 def start_server(port=8000):
-    logger.info("start_server()")
-
-    app = falcon.App(cors_enable=True)
-    queues = QueueResource()
-    app.add_route('/queues/{queue_id}', queues)
-
-    app.add_static_route('/assets', f"{PACKAGE_ASSETS_DIR}")
-
-    with make_server('127.0.0.1', port, app) as httpd:
-        logger.info(f'Starting server on port {port}')
-        httpd.serve_forever()
+    logger.info(f'Starting server on port {port}')
+    app.run(host='127.0.0.1', port=port)
 
 def find_available_port() -> int:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
