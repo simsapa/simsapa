89ac942f5f7d280cf352a1cb059893d4693dde85
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2022-11-03 18:39:30 +0000

    nuitka build

---
+++ nuitka_build.bat
@@ -0,0 +1,14 @@
+
+::  --disable-console
+
+python -m nuitka ^
+  --output-dir=dist_nuitka ^
+  --standalone ^
+  --follow-imports ^
+  --enable-plugin=numpy ^
+  --include-module="sqlalchemy.sql.default_comparator" ^
+  --windows-icon-from-ico="simsapa\assets\icons\appicons\simsapa.ico" ^
+  --include-data-dir="simsapa\assets=simsapa\assets" ^
+  --include-data-dir="simsapa\alembic=simsapa\alembic" ^
+  --include-data-files="simsapa\alembic.ini=simsapa\alembic.ini" ^
+  run.py
