ba87aa5d3bf057e132708c263dfd52d796ee08e7
Gambhiro <gambhiro@users.noreply.github.com>
2022-11-06 06:54:38 +0000

    upd build actions

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -6,9 +6,12 @@ if [ -d build ]; then rm -r build; fi
 if [ -d dist ]; then rm -r dist; fi
 
 pyinstaller run.py \
-    --name simsapa \
-    --onefile \
+    --name "Simsapa Dhamma Reader" \
+    --onedir \
     -w \
-    --add-data simsapa/assets:simsapa/assets \
-    --add-data simsapa/alembic:simsapa/alembic \
-    --add-data simsapa/alembic.ini:simsapa/alembic.ini
+    --clean \
+    --noupx \
+    -i "simsapa/assets/icons/appicons/simsapa.ico" \
+    --add-data "simsapa/assets:simsapa/assets" \
+    --add-data "simsapa/alembic:simsapa/alembic" \
+    --add-data "simsapa/alembic.ini:simsapa/alembic.ini"
