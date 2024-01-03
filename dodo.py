4bcbd98fa2a982b06dfd7d80fb0859b815d67c36
Gambhiro <gambhiro@users.noreply.github.com>
2024-01-03 21:30:30 +0100

    doit tasks

--- dodo.py
+++ dodo.py
@@ -34,7 +34,11 @@ def task_update_build_open():
             'poetry run doit build',
             open_cmd,
         ],
-        'params': [{'name': 'branch', 'long': 'branch', 'default': 'main', 'help': 'Specify the git branch.'}],
+        'params': [{'name': 'branch',
+                    'long': 'branch',
+                    'default': 'main',
+                    'type': str,
+                    'help': 'Specify the git branch.'}],
         'title': title_with_actions,
     }
 
@@ -164,3 +168,68 @@ def update_release_channel(db_path: Path, release_channel: str):
 
     conn.commit()
     conn.close()
+
+def task_bootstrap_db():
+    return {
+        'actions': [""" ./scripts/bootstrap_db.sh """],
+    }
+
+def task_simsapa_min_js():
+    return {
+        'actions': [""" npx webpack """],
+        'targets': ["simsapa/assets/js/simsapa.min.js"],
+        'clean': True,
+    }
+
+def task_requirements_txt():
+    return {
+        'actions': [""" poetry export --without-hashes -o requirements.txt """],
+        'targets': ['requirements.txt'],
+        'clean': True,
+    }
+
+def task_icons():
+    return {
+        'actions': [""" ./scripts/icons_assets.sh """],
+    }
+
+def ui_to_py():
+    cmd = """
+pyuic6 -o simsapa/assets/ui/sutta_search_window_ui.py simsapa/assets/ui/sutta_search_window.ui && \
+pyuic6 -o simsapa/assets/ui/sutta_study_window_ui.py simsapa/assets/ui/sutta_study_window.ui && \
+pyuic6 -o simsapa/assets/ui/dictionary_search_window_ui.py simsapa/assets/ui/dictionary_search_window.ui && \
+pyuic6 -o simsapa/assets/ui/memos_browser_window_ui.py simsapa/assets/ui/memos_browser_window.ui && \
+pyuic6 -o simsapa/assets/ui/links_browser_window_ui.py simsapa/assets/ui/links_browser_window.ui && \
+pyuic6 -o simsapa/assets/ui/dictionaries_manager_window_ui.py simsapa/assets/ui/dictionaries_manager_window.ui && \
+pyuic6 -o simsapa/assets/ui/document_reader_window_ui.py simsapa/assets/ui/document_reader_window.ui && \
+pyuic6 -o simsapa/assets/ui/library_browser_window_ui.py simsapa/assets/ui/library_browser_window.ui && \
+pyuic6 -o simsapa/assets/ui/import_stardict_dialog_ui.py simsapa/assets/ui/import_stardict_dialog.ui
+    """
+
+    return {
+        'actions': [cmd],
+    }
+
+def task_sass_build():
+    # Ruby 3.0 sass, version:
+    # 1.49.7 compiled with dart2js 2.15.1
+    return {
+        'actions': [""" sass --no-source-map './simsapa/assets/sass/:./simsapa/assets/css/' """]
+    }
+
+def task_sass_watch():
+    return {
+        'actions': [""" sass --no-source-map --watch './simsapa/assets/sass/:./simsapa/assets/css/' """]
+    }
+
+def task_count_code():
+    return {
+        'actions': [""" tokei --type Python --exclude simsapa/assets/ --exclude simsapa/keyboard/ --exclude simsapa/app/lookup.py . | grep -vE '===|Total' """],
+    }
+
+def task_profile_time_chart_png():
+    return {
+        'actions': [""" gnuplot < scripts/profile_time_chart.gp """],
+        'targets': ['profile_time_chart.png'],
+        'clean': True,
+    }
