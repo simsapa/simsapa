7cffcdb45fa1f9f2e613d360fac76852636994c3
Gambhiro <gambhiro@users.noreply.github.com>
2026-07-31 09:47:53 +0100

    v1.0.0-alpha.4

--- cpp/gui.cpp
+++ cpp/gui.cpp
@@ -457,7 +457,7 @@ int start(int argc, char* argv[]) {
     free_rust_string(desktop_file_path);
   }
 
-  app.setApplicationVersion("v1.0.0-alpha.3");
+  app.setApplicationVersion("v1.0.0-alpha.4");
 
   // app_windows = AppWindows(app, app_data, hotkeys_manager, enable_tray_icon)
 
