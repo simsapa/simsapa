a1f8cbcd2e3b49b993d4662363dcd474fff89969
Gambhiro <gambhiro@users.noreply.github.com>
2026-08-14 10:28:16 +0100

    v1.0.0-alpha.6

--- cpp/gui.cpp
+++ cpp/gui.cpp
@@ -667,7 +667,7 @@ int start(int argc, char* argv[]) {
     free_rust_string(desktop_file_path);
   }
 
-  app.setApplicationVersion("v1.0.0-alpha.5");
+  app.setApplicationVersion("v1.0.0-alpha.6");
 
   // app_windows = AppWindows(app, app_data, hotkeys_manager, enable_tray_icon)
 
